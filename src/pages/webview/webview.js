644a6e33338991a5ec8334ca4bed3f3e53e49400
chengjinrui <chengjinrui@babyfs.cn>
2019-05-27 09:05:48 +0800

    doubleLeaving init

---
+++ src/pages/webview/webview.js
@@ -0,0 +1,79 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // src: 'https://sulfurous.aau.at/html/app.html?id=15832807&turbo=false&full-screen=false&aspect-x=&aspect-y=&resolution-x=1334&resolution-y=750'
+    src: 'http://pr0y5dlb6.bkt.clouddn.com/index1.html'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  onResize(res) {
+    console.log(res.size.windowWidth); // 新的显示区域宽度
+    console.log(res.size.windowHeight); // 新的显示区域高度
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享(用户分享时可获取当前<web-view/>的URL)
+   */
+  onShareAppMessage(options) {
+
+  },
+
+  /**
+   * h5与小程序通信 version > 1.7.1
+   */
+  onPostMessage(e) {
+
+  },
+
+  customData: {}
+});
